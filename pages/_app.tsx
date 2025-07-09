f77a7cde8a6703e16f47f165394ac014cb7eedf5
Insanity44 <JamesJabbour22@gmail.com>
2025-07-09 23:14:58 +0300

    Fix: Added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,23 @@
-// pages/_app.tsx
 import "../styles/globals.css";
 import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
-
       {/* Google Analytics */}
-      {/* Load gtag.js dynamically */}
       <Script
-        id="google-gtag"
+        src="https://www.googletagmanager.com/gtag/js?id=G-7J5M99WEQ1"
         strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-3XJ4TKEYW6"
       />
-
-      {/* Initialize gtag with your Measurement ID */}
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-3XJ4TKEYW6');
+          gtag('config', 'G-7J5M99WEQ1');
         `}
       </Script>
+      <Component {...pageProps} />
     </>
   );
 }
