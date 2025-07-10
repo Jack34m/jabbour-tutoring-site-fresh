210965a930cad98a54a9074974e4f3ee31a1835b
Insanity44 <JamesJabbour22@gmail.com>
2025-07-10 17:44:31 +0300

    Fix: still fixing favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,18 @@ export default function Document() {
       <Head>
         {/* Favicons */}
         <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <meta name="theme-color" content="#0f3d2e" />
       </Head>
       <body>
